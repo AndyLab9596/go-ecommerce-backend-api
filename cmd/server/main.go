67f7746506ba52f5b436f13fe5a6887a738f38ef
vyct1 <thienvy95@gmail.com>
2025-05-12 09:01:43 +0700

    GO FRESHER (3): GIN vs ROUTER

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"github.com/AndyLab9596/go-ecommerce-backend-api/internal/routers"
+)
 
 func main() {
-	fmt.Println("Starting")
+	r := routers.NewRouter()
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
