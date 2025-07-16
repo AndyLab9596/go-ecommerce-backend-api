8b871d176f25fe549c7bd197497d1b2ac2538603
MAC <mac@192.168.2.242>
2025-07-16 15:44:47 +0700

    ok

--- internal/controller/pong.controller.go
+++ internal/controller/pong.controller.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,9 +17,18 @@ func NewPongController() *PongController {
 func (p *PongController) Pong(c *gin.Context) {
 	name := c.Param("name")
 	uid := c.Query("uid")
+	age := c.DefaultQuery("age", "30")
+
+	ageInt, err := strconv.Atoi(age)
+	if err != nil {
+		fmt.Println("convert age error::", err)
+		ageInt = 0
+	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong " + name,
 		"uid":     uid,
+		"age":     ageInt,
+		"users":   []string{"cr7", "m10"},
 	})
 }
